7e770aa7ca529b7c086d2b74d5eeb5d8ffef5720
Alexander Pavlov <74762262+alexpavlo@users.noreply.github.com>
2022-10-28 10:36:26 +0600

    -

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-20'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto eum repudiandae quaerat illo rem totam labore doloremque aut vel iste, fugiat praesentium eveniet excepturi, possimus impedit. Consequatur debitis tempora totam odio? Natus quibusdam corrupti possimus perspiciatis dolores rerum iste in, culpa fugit enim quaerat laboriosam! Doloremque delectus recusandae deleniti ipsa!</p>
+            <br/>
+            <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nobis vel accusantium exercitationem tenetur eius qui porro iure unde reprehenderit facilis corrupti cupiditate, accusamus at, magnam laborum cumque suscipit maiores odit dolorum maxime, dolore recusandae. Enim ullam quis assumenda nobis error ipsam eveniet in sunt odit? Consequatur, veritatis sapiente! Quod!</p>
+
+        </div>
+    </div>
+  )
+}
+
+export default About
